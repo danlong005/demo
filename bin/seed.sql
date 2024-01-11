8b4f0b606c8c82b917ba2f1c5aaa2373d3b3b1aa
Daniel Long <danlong5@gmail.com>
2024-01-11 04:33:40 -0600

    remove junk

--- bin/seed.sql
+++
@@ -1,7 +0,0 @@
-
-insert into employees 
-    values(default, 'Daniel', 'Long', current_timestamp, 100000.00, '1982-10-09', current_timestamp, current_timestamp);
-    
-insert into employees 
-    values(default, 'Blair', 'Long', current_timestamp, 120000.00, '1981-12-14', current_timestamp, current_timestamp);
-    
